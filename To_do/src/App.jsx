dfd3f6cbef0722b42a46fd006b85ca866062db9b
Matheus Figueiredo <matheus.figueiredo397@gmail.com>
2025-07-29 22:33:05 -0400

    Finish front-end

--- To_do/src/App.jsx
+++ To_do/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Todo from './components/Todo';
 import TodoForm from './components/TodoForm';
 import Search from './components/Search';
+import Filter from './components/Filter';
 
 function App() {
   const [todos, setTodos] = useState([
@@ -27,6 +28,9 @@ function App() {
   ]);
 
   const [search, setSearch] = useState('');
+
+  const [filter, setFilter] = useState('all');
+  const [sort, setSort] = useState('asc');
 
   const addTodo = (text, category) => {
     const newTodo = {
@@ -58,10 +62,23 @@ function App() {
     <div className="app">
       <h1>To-Do List</h1>
       <Search search={search} setSearch={setSearch} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
       <div className="todo-list">
         {todos
           .filter((todo) =>
+            filter === 'all'
+              ? true
+              : filter === 'completed'
+              ? todo.completed
+              : !todo.completed
+          )
+          .filter((todo) =>
             todo.text.toLowerCase().includes(search.toLocaleLowerCase())
+          )
+          .sort((a, b) =>
+            sort === 'asc'
+              ? a.text.localeCompare(b.text)
+              : b.text.localeCompare(a.text)
           )
           .map((todo) => (
             <Todo
---
+++ To_do/src/components/Filter.jsx
@@ -0,0 +1,24 @@
+const Filter = ({ filter, setFilter, setSort }) => {
+  return (
+    <div className="filter">
+      <h2>Filter:</h2>
+      <div className="filter-options">
+        <div>
+          <p>Status:</p>
+          <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="pending">Pending</option>
+          </select>
+        </div>
+        <div>
+          <p>alphabetical order</p>
+          <button onClick={() => setSort('asc')}>Asc</button>
+          <button onClick={() => setSort('desc')}>Desc</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
